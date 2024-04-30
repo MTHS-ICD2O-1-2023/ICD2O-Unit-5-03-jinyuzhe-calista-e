de025f8b5c2eee25d789f385a1b73d2d0103204b
Calista.E <159185314+jinyuzhe-calista-e@users.noreply.github.com>
2024-04-30 18:51:20 +0000

    wdc

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Calista.E
+// Created on: Apr 2024
 // This file contains the JS functions for index.html
+"use strict"
+
+function calculate() {
+  const age = parseInt(document.getElementById("age").value)
+
+if (age >= 18) {
+  document.getElementById("answer").innerHTML =
+    "You can see an R movie alone."
+} else if (age >=13) {
+  document.getElementById("answer").innerHTML =
+    "You can see a PG-13 movie alone."
+} else if (age <= 12) {
+  document.getElementById("answer").innerHTML =
+}    "You can see a G or PG movie alone."
+}
